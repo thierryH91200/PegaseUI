a9af06c40cf18925f9cae3beebafd63522c73062
thierryH91200 <thierry.hentic@gmail.com>
2024-10-31 09:29:09 +0100

    com241031A

---
+++ PegaseUI/Group/Rapport/RubriquePieView.swift
@@ -0,0 +1,8 @@
+//
+//  RubriquePieView.swift
+//  PegaseUI
+//
+//  Created by Thierry hentic on 31/10/2024.
+//
+
+import Foundation
---
+++ PegaseUI/exemple5.swift
@@ -0,0 +1,8 @@
+//
+//  exemple5.swift
+//  PegaseUI
+//
+//  Created by Thierry hentic on 30/10/2024.
+//
+
+import Foundation
